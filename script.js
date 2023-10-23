4d448393cf772db2915b4fdc9bc8916e4da37026
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2023-10-23 13:06:58 +0100

    Refactor code to work with DOM

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const btn = document.querySelectorAll(".btn");
+const result = document.querySelector(".result");
+const finalResult = document.querySelector(".final-result");
+const playerScoreCount = document.querySelector(".player-score");
+const computerScoreCount = document.querySelector(".computer-score");
+
+btn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    playRound(event.target.textContent, getComputerChoice);
+    playerScoreCount.textContent = playerScore;
+    computerScoreCount.textContent = computerScore;
+  });
+});
+
 // Returns a random choice of the computer from an array
 const getComputerChoice = () => {
   let result = ["Rock", "Paper", "Scissors"];
@@ -12,12 +26,14 @@ const capitalize = (word) => {
 
 let playerScore = 0;
 let computerScore = 0;
+playerScoreCount.textContent = playerScore;
+computerScoreCount.textContent = computerScore;
 
 // Returns a string showing a win or loss and adds to the winner's score for a round
 const playRound = (playerSelection, computerSelection) => {
   computerSelection = getComputerChoice();
-  playerSelection = prompt("Rock, Paper or Scissors?");
-  playerSelection = capitalize(playerSelection);
+  // playerSelection = prompt("Rock, Paper or Scissors?");
+  // playerSelection = capitalize(playerSelection);
   console.log(playerSelection);
   console.log(computerSelection);
 
@@ -25,32 +41,28 @@ const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "" || playerSelection === undefined) {
     alert("Invalid Selection!!!");
   } else if (playerSelection === computerSelection) {
-    return `It's a draw!!! ${playerSelection} ties with ${computerSelection}`;
+    result.textContent = `It's a draw!!! ${playerSelection} ties with ${computerSelection}`;
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
     playerScore++;
-    return `You Win!!! ${playerSelection} beats ${computerSelection}`;
+    result.textContent = `You Win!!! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
     playerScore++;
-    return `You Win!!! ${playerSelection} beats ${computerSelection}`;
+    result.textContent = `You Win!!! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
     playerScore++;
-    return `You Win!!! ${playerSelection} beats ${computerSelection}`;
+    result.textContent = `You Win!!! ${playerSelection} beats ${computerSelection}`;
   } else {
     computerScore++;
-    return `You Lose!!! ${computerSelection} beats ${playerSelection}`;
+    result.textContent = `You Lose!!! ${computerSelection} beats ${playerSelection}`;
   }
+  gameWinner();
 };
 
-// Keep scores and returns winner of 5 rounds
-const game = () => {
-  for (let i = 1; i <= 5; i++) {
-    console.log(playRound());
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-  }
-  if (computerScore > playerScore) {
-    return "Game Over!!!";
-  } else {
-    return "Victory is yours!!!";
+// Returns winner of 5 rounds
+const gameWinner = () => {
+  if (computerScore === 5) {
+    finalResult.textContent = "You Lose! Game Over!!!";
+  } else if (playerScore === 5) {
+    finalResult.textContent = "You Win! Victory is yours!!!";
   }
 };
